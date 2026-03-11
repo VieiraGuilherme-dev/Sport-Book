f07e22129dbf162f36c67a06b1b2660d335beb4b
VieiraGuilherme-dev <gv524003@gmail.com>
2026-03-11 11:06:32 -0300

    Project Created

---
+++ court-service/src/test/java/com/sportbook/courtservice/CourtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportbook.courtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
