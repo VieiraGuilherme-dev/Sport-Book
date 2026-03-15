eeedc1de9aa2c83039b57d996ef139f00fb5bccb
VieiraGuilherme-dev <gv524003@gmail.com>
2026-03-15 16:02:47 -0300

    initial setup booking-service - pom.xml, application.yml and package structure

---
+++ booking-service/src/test/java/com/sportbook/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportbook.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
